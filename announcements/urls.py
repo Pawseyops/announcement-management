c172b6e0449f30e8c82c36b8b0ad6d07f3403f99
ensignexpendable <mark@ensignexpendable.com>
2015-08-20 10:32:14 +0800

    refixing urls

--- announcements/urls.py
+++ announcements/urls.py
@@ -17,9 +17,10 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic.base import RedirectView
 from django.core.urlresolvers import reverse_lazy
+from django.conf import settings
 
 urlpatterns = [
     url(r'^announce/', include('announcements.announce.urls', namespace="announce")),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^.*$', RedirectView.as_view(url=reverse_lazy(announcements.announce.views.IndexView), permanent=False), name='index')
+    url(r'^.*$', RedirectView.as_view(url=('%s/%s'%(settings.MYURL,'announce/')), permanent=False), name='redirect_to_index')
 ]
